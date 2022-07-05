fa29cf6a01429de11b71017cce589a783a8fd366
emiliazm <98142626+emiliazm@users.noreply.github.com>
2022-07-05 20:59:55 +0200

    Add queries

--- queries.sql
+++ queries.sql
@@ -15,3 +15,27 @@ SELECT * from animals WHERE neutered = true;
 SELECT * from animals WHERE name <> 'Gabumon';
 
 SELECT * from animals WHERE weight_kg between 10.4 and 17.3;
+
+SELECT COUNT(*)
+FROM animals;
+
+SELECT 
+COUNT(escape_attempts)
+FROM animals
+WHERE escape_attempts < 1;
+
+SELECT AVG(weight_kg)
+FROM animals;
+
+SELECT name, escape_attempts
+FROM animals
+WHERE escape_attempts = (SELECT MAX(escape_attempts) from animals);
+
+SELECT species, MAX(weight_kg), MIN(weight_kg)
+FROM animals
+GROUP BY species;
+
+SELECT species, AVG(escape_attempts)
+FROM animals
+WHERE date_of_birth between '1990-01-01' and '2000-12-31'
+GROUP BY species;
